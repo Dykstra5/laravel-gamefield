67028b6b3537fa8d04e8e4ed189da25fc6ce33c2
Dykstra5 <moreno_ismael2000@hotmail.com>
2024-06-10 11:21:01 +0200

    ultimos cambios (espero)

--- resources/js/Pages/Search/View.vue
+++ resources/js/Pages/Search/View.vue
@@ -61,21 +61,21 @@ const props = defineProps({
                             </div>
                             <div v-if="genres.length > 0">
                                 <h3 class="text-xl text-left">Géneros</h3>
-                                <a :href="route('tag', { type: 'game', slug: tag.slug })" v-for="tag of genres"
+                                <a :href="route('tag', { type: 'genre', slug: tag.slug })" v-for="tag of genres"
                                     class="my-1 p-1 rounded-md bg-red-700 hover:bg-red-600 hover:underline block text-white">{{
                                         tag.name
                                     }}</a>
                             </div>
                             <div v-if="platforms.length > 0">
                                 <h3 class="text-xl text-left">Plataformas</h3>
-                                <a :href="route('tag', { type: 'game', slug: tag.slug })" v-for="tag of platforms"
+                                <a :href="route('tag', { type: 'platform', slug: tag.slug })" v-for="tag of platforms"
                                     class="my-1 p-1 rounded-md bg-pink-700 hover:bg-pink-600 hover:underline block text-white">{{
                                         tag.name
                                     }}</a>
                             </div>
                             <div v-if="developers.length > 0">
                                 <h3 class="text-xl text-left">Desarrolladores</h3>
-                                <a :href="route('tag', { type: 'game', slug: tag.slug })" v-for="tag of developers"
+                                <a :href="route('tag', { type: 'developer', slug: tag.slug })" v-for="tag of developers"
                                     class="my-1 p-1 rounded-md bg-fuchsia-700 hover:bg-fuchsia-600 hover:underline block text-white">{{
                                         tag.name
                                     }}</a>
